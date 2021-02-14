9821f4204cec1721da4e4216474a0febefc63803
Michał Kopacz <kopaczmichal093@gmail.com>
2021-02-14 18:45:28 +0100

    Add validation if user entered both username and password

--- app.js
+++ app.js
@@ -7,36 +7,41 @@ route('/', 'home', function() {
     e.preventDefault();
     const login = e.target.login.value;
     const pass = e.target.password.value;
-    const payload = {
-      username: login,
-      password: pass,
-    };
+    if (!login || ! pass) {
+      alert('Please enter both username and password correctly!');
+      this.$refresh();
+    } else {
+      const payload = {
+        username: login,
+        password: pass,
+      };
 
-    fetch('https://zwzt-zadanie.netlify.app/api/login', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(payload)
-    })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (parsedResponse) {
-          verifyData(parsedResponse.message);
-        })
-        .catch(err => {
-          //log below is for developing purpose on errors :)
-          console.log('err:',err)
-        });
+      fetch('https://zwzt-zadanie.netlify.app/api/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(payload)
+      })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (parsedResponse) {
+            verifyData(parsedResponse.message);
+          })
+          .catch(err => {
+            //log below is for developing purpose on errors :)
+            console.log('err:',err)
+          });
 
-    const verifyData = (res) => {
-      if (res === 'Login success!') {
-        window.location.href="#/success";
-      } else if (res === 'Wrong password!') {
-        alert('Login Failed')
-        this.$refresh();
+      const verifyData = (res) => {
+        if (res === 'Login success!') {
+          window.location.href="#/success";
+        } else if (res === 'Wrong password!') {
+          alert('Login Failed')
+          this.$refresh();
+        }
       }
     }
   })
